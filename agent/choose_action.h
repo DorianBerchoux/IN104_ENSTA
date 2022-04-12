81182a303bdda6fd1a0b84816b885408322b8046
Dorian BERCHOUX <dorian.berchoux@ensta-paris.fr>
2022-04-12 16:02:04 +0200

    modif

---
+++ agent/choose_action.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "Qlearning.h"
+#include "mazeEnv.h"
+ 
+#define alpha=0.5;
+#define eps=0.3;
+#define gamma=0.3;
