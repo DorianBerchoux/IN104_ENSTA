c3ae329aca082d6f139b852651ee93824ca28eb7
Dorian BERCHOUX <dorian.berchoux@ensta-paris.fr>
2022-04-12 16:31:12 +0200

    yo bg j'ai fini les grandes lignes de choose_action et j'ai supprimé les anciens fichiers

--- agent/choose_action.c
+++ agent/choose_action.c
@@ -2,12 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include "Qlearning.h"
 #include "mazeEnv.h"
- 
-#define alpha=0.5;
-#define eps=0.3;
-#define gamma=0.3;
+#include "choose_action.h"
 
 double randdouble(){
     srand(time(NULL));
@@ -31,7 +27,7 @@ int maxQ(){
 }
 
 
-action Q_eps_greedy(double eps){
+action Q_eps_greedy(){
 //on choisit l'action à l'aide de la méthode eps-greedy
 
 //on commence par choisir un nombre aléatoirement entre 0 et 1
@@ -45,14 +41,34 @@ action Q_eps_greedy(double eps){
         //on a un nombre entre 0 et 2, soit 4 valeurs possibles.
         return move;
     }else{
-        int colmax=maxQ(s);
+        int colmax=maxQ();
         return colmax;
         //on choisit l'action qui va maximiser notre récompense
     }
 }
 
-action Q_blotzmann(action a){
+action Q_blotzmann(){
     double sum=0.0;
-    //on va stocker ici la somme des exp(Q(s,a)) pour en déduire des probabilités
-    
+    //on va stocker ici la somme des exp(Q(s,a)) pour en déduire les 4 différentes probabilités 
+    for (int i=0;i<4;++i){
+        int row=state_row*cols+state_col;//on calcule la ligne correspondante à notre état s de notre matrice Q
+        sum=sum+exp(Q[row][i]);
+    }
+    //on calcule les probas 
+    double* proba[4];
+    for (int i=0;i<4;++i){
+        proba[i]=exp(Q[row][i]/sum);
+    }
+    //on choisit un nombre entre 0 et 1 : s'il est entre 0 et proba[0], on choisit l'action 0 ; s'il est entre proba[0] et proba[1], on fait l'action 1 ...
+    double p=randdouble();
+    if (p<proba[0]){
+        return up;
+    }else if (p<proba[1]+proba[0]){
+        return down;
+    }else if (p<proba[2]+proba[1]+proba[0]){
+        return left;
+    }else{ 
+        return right;
+    }
 }
+
