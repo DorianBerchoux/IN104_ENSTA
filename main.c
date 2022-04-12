ab18e7a63ca63c5d79332a4f717cb188e8343079
Ladislas de Francqueville <ladislas.de-francqueviille@ensta-paris.fr>
2022-04-12 16:41:37 +0200

    on a viré le main de qlearning.c

---
+++ main.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+
+#include"qlearning.h"
+#include "mazeEnv.h"
+#include "functions.h"
+#include "choose_action.h"
+
+
+int main(){
+
+	maze_make(); //On met quel filename ?
+
+	maze_reset(); //on part de (start_row,start_col)
+
+	float** Q = makeQ();
+
+	while((state_row != goal_row) && (state_col!=goal_col)){
+
+		action a = Q_eps_greedy(eps);
+
+		float r = recompense(a); //faut modifier la fonction récompense pour qu'elle prenne que a en paramètre !!
+
+
+		actualisationQ(alpha,gamma,Q,state_row,state_col,a);
+		
+
+		actualisation_position(a); //actualise state_row/col 
+	}
+
+	//On part d'un état s initial (ie une position initiale)
+	//On choisit une action au hasard
+	//On calcule la récompense de la case s' où on se rend
+	//On actualise la valeur de Q(s,a), associée à la case ou se trouve AVANT de faire l'action
+	//On actualise la positon s<-s' en effectuant le déplacement q'implique l'action 
+	//On continue tant que s != goal.
+
+
+	//on est sur un état s, on choisit une action, on actualise Q puis on actualise la position.
+
