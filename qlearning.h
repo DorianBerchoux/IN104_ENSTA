dbae7ce3d20ce1e03e98c35c4cdcb5eda0ad2079
Ladislas de Francqueville <ladislas.de-francqueviille@ensta-paris.fr>
2022-05-22 22:26:30 +0200

    tentative d'implémenter doubleQ

--- qlearning.h
+++ qlearning.h
@@ -15,6 +15,10 @@ float recompense(action a );
 
 void actualisationQ (float gamma, float alpha, float** Q, action a, float r);
 
+void double_actualisationQ (float gamma, float alpha, float** Q1,float** Q2, action a, float r);
+
+
+
 void actualisation_position(action a);
 
 
